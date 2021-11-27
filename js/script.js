2fc27e96ca81f07f7484742b81d4a4c4f6a944e4
samskaufel <85969886+samskaufel@users.noreply.github.com>
2021-11-26 20:19:08 -0800

    created a to-do-list app using jQuery

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// jQuery Code
+//Adding a new item to the list
+
+function newItem() {
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+  // Crossing an item out
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+  // Adding a delete button
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+  $("#list").sortable();
+}
